78511d24f0c3e7afa5d0fe5829bf5cc203c7af1f
张葛 <helloworld_zac@163.com>
2017-03-17 08:12:05 +0800

    开始quartz学习

---
+++ spring/src/main/java/spring/quartz/XmlTimer.java
@@ -0,0 +1,18 @@
+package spring.quartz;
+
+import org.slf4j.Logger;
+
+/**
+ * Xml配置方式.
+ *  
+ * @author Zhang.Ge
+ * @version v1.0 2017年3月17日  上午8:04:28
+ */
+public class XmlTimer {
+
+    Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
+    
+    public void execute() {
+        log.info("执行");
+    }
+}
---
+++ spring/src/main/java/spring/quartz/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 一句话功能简述.
+ * 
+ * <pre>
+ * 功能详细描述
+ * </pre>
+ *  
+ * @author Zhang.Ge
+ * @version v1.0 2017年3月17日  上午8:04:17
+ */
+package spring.quartz;
